ea22da353b33c8a795e588456f4750cb595fba5d
kevin huang <1012956714@qq.com>
2019-03-24 14:20:03 +0800

    修改2-thunk.js

--- demo-generator/1-fibG.js
+++ demo-generator/1-fibG.js
@@ -1,4 +1,4 @@
-function fib (max) { // 斐波数列产生器
+function fib (max) { // 斐波那契数列产生器
   let a = 0
   let b = 1
   let arr = [0, 1]
@@ -24,6 +24,8 @@ function * fibG (max) {
 }
 
 // 调用方法1
+console.log('---------------------------------')
+console.log('调用generator的next()')
 let f = fibG(5)
 console.log(f.next()) // 0
 console.log(f.next()) // 1
@@ -33,6 +35,8 @@ console.log(f.next()) // 3
 console.log(f.next()) // undefined
 
 // 调用方法2
-for (let x of fibG(5)) {
+console.log('--------------------------------')
+console.log('用for of遍历generator函数')
+for (let x of fibG(5)) { // generator函数可遍历
   console.log(x) // 依次输出 0 1 1 2 3
 }
--- demo-generator/2-thunk.js
+++ demo-generator/2-thunk.js
@@ -3,6 +3,19 @@
 // 这个临时函数就叫Thunk函数
 const fs = require('fs')
 
+// 函数原型：fs.readFile(file[, options], callback)。支持3个参数，其中最后一个是回调函数
+// 普通调用方式：
+// function someCallback(err, data) {
+//     if (err) throw err;
+//     console.log(data);
+// }
+// fs.readFile('./oranges.txt','utf8', someCallback);
+
+function someCallback (err, data) {
+  if (err) throw err
+  console.log(data)
+}
+
 const thunk = function (fileName, codeType) { // thunk返回一个回调函数
   return function (callback) {
     fs.readFile(fileName, codeType, callback)
@@ -13,6 +26,7 @@ const readFileThunk = thunk('../data.json', 'utf-8')
 
 // 经过处理，只接受回调函数作为参数，这个单参数版本，就叫做Thunk函数
 // 任何函数，只要参数有回调函数，就能写成 Thunk 函数的形式
-readFileThunk((err, data) => { // readFileThunk只接受一个回调函数为参数，它就是Thunk函数
-  console.log(data)
-})
+readFileThunk(someCallback) // readFileThunk只接受一个回调函数为参数，它就是Thunk函数
+
+// Thunk函数真正的作用是简化了参数，将原本多参的函数，简化成只接受回调函数做参数。
+// 即多参版本的异步函数，经由Thunk，变成了单参（参数为回调函数）函数。
