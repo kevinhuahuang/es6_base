8d291d7eadd1e70bbac4e920f1546dac46fe5ab6
kevin huang <1012956714@qq.com>
2019-03-26 15:04:50 +0800

    '修改文件6-for'

--- demo-async/3-async-request.js
+++ demo-async/3-async-request.js
@@ -1,4 +1,5 @@
-// 用setTimeout来模拟异步请求
+// 你有三个请求需要发生，第三个请求是依赖于第二个请求的解构第二个请求依赖于第一个请求的结果。
+// 若用 ES5实现会有3层的回调，若用Promise 实现至少需要3个then。一个是代码横向发展，另一个是纵向发展
 function sleep (second, param) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
---
+++ demo-async/6-for.js
@@ -0,0 +1,16 @@
+// 正常 for 循环
+async function forDemo () {
+  let arr = [1, 2, 3, 4, 5]
+  for (let i = 0; i < arr.length; i++) {
+    await arr[i]
+  }
+}
+console.log(forDemo())// 正常输出
+
+async function forBugDemo() {
+    let arr = [1, 2, 3, 4, 5];
+    arr.forEach(item => {
+        await item; // 报错 await 不在 async的上下文
+    }, this);
+}
+forBugDemo();// Uncaught SyntaxError: Unexpected identifier
