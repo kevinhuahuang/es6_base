359e628f1b54b683df176cc4e4deb77e56f164ec
kevin huang <1012956714@qq.com>
2019-03-30 11:52:33 +0800

    '修改02-enumerate.js'

--- demo-array/02-enumerate.js
+++ demo-array/02-enumerate.js
@@ -1,17 +1,20 @@
 let aryPublic = [1, 2, 3, 4, 5, 6, , , 7, 8, 9, 10, 11]
 
 // find 返回第一个符合条件的元素
+console.log('----------find')
 console.log(aryPublic.find((value, index, ary) => {
   return value > 3
 }))
 
 // findIndex 返回第一个符合条件的元素的index
+console.log('----------findIndex start from 0')
 console.log(aryPublic.findIndex((value, index, ary) => {
   return value > 3
 }))
 
 // ==============================================================
 // for of --keys()  values() entries()
+console.log('----------keys')
 for (let index of aryPublic.keys()) {
   console.log('index:' + index)
 }
@@ -20,28 +23,46 @@ for (let index of aryPublic.keys()) {
 //   console.log('value:' + value)
 // }
 
+console.log('----------entries 键值对保存为数组')
 for (let entry of aryPublic.entries()) {
-  console.log('entry:' + entry)
+  console.log(entry)
 }
 
+console.log('----------entries  index : value')
 for (let [index, value] of aryPublic.entries()) {
   console.log(index + ' : ' + value)
 }
 
 // ==============================================================
+// forEach
+console.log('----------forEach')
+let indexAry = []
+let valueAry = []
+aryPublic.forEach((value, index, ary) => { // 跳过空值，但空值占用index
+  indexAry.push(index) // 缺少6，7两个空值占用的index
+  valueAry.push(value)
+})
+
+console.log(indexAry)
+console.log(valueAry)
+// ==============================================================
 // every some filter map
+console.log('----------every')
 console.log(aryPublic.every((value, index, ary) => {
   return value > 1
 }))
 
+console.log('----------some')
 console.log(aryPublic.some((value, index, ary) => {
   return value > 10
 }))
 
+console.log('----------filter')
 console.log(aryPublic.filter((value, index, ary) => {
   return value > 5
 }))
 
+console.log('----------map')
 console.log(aryPublic.map((value, index, ary) => {
   return value + ''
 }))
