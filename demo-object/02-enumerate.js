dc576b42edef62005355acdc1b10194782caf7e2
kevin huang <1012956714@qq.com>
2019-03-30 12:05:27 +0800

    '增加02-enumerate'

---
+++ demo-object/02-enumerate.js
@@ -0,0 +1,33 @@
+let objPublic = {
+    name: 'kevin',
+    height: 175,
+    weight: '70kg',
+    age: 35,
+    expect: {
+        weight: '65kg'
+    },
+    [Symbol('hair')]: 'short'
+}
+
+console.log('------------------for in')
+for (let key in objPublic) {
+    console.log(key)
+}
+
+console.log('------------------Object.keys(obj)')
+for (let key of Object.keys(objPublic)) {
+    console.log(key)
+}
+
+console.log('------------------Object.values(obj)')
+for (let key of Object.values(objPublic)) {
+    console.log(key)
+}
+
+console.log('------------------Object.entries(obj) 键值对用数组保存')
+for (let key of Object.entries(objPublic)) {
+    console.log(key)
+}
+
+console.log('------------------Object.key(obj) 返回属性名组成的数组')
+console.log(Object.keys(objPublic))
